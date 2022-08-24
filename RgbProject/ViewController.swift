1d12b62818f52852cf0cbc136c1b5941a72ff9f1
1nvis1ble <alexeyzyamin@gmail.com>
2022-08-24 15:36:24 +0300

    Initial Commit

---
+++ RgbProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RgbProject
+//
+//  Created by Aleksey Zyamin on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
